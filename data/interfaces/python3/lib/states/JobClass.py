bd0a4a22c0e5e4a7d1f15cc7f2e1434f3b649c40
madkne-laptop <madkne@laptop>
2022-08-23 05:57:22 +0430

    upgrade job class to interface

--- data/interfaces/python3/lib/states/JobClass.py
+++ data/interfaces/python3/lib/states/JobClass.py
@@ -1,6 +1,6 @@
 
 import json
-from typing import Dict, Literal
+from typing import Dict, Literal, Optional
 
 
 class WorkflowStateJob():
@@ -11,14 +11,30 @@ class WorkflowStateJob():
     __action_name: str
     __state_name: str
 
-    def __init__(self, id: str, repeat: int, time: Dict, action_name: str, state_name: str):
+    def __init__(self, id: Optional[str], repeat: Optional[int], action_name: Optional[str], state_name: Optional[str]):
         self.__id = id
         self.__repeat = repeat
-        self.__time = time
         self.__action_name = action_name
         self.__state_name = state_name
 
         return None
+
+    def time(self, timestamp: Optional[int], timestamp_field: Optional[str], hour: Optional[int], minute: Optional[int], second: Optional[int], day: Optional[int]):
+        self.__time = {}
+        if timestamp is not None:
+            self.__time['timestamp'] = timestamp
+        if timestamp_field is not None:
+            self.__time['timestamp_field'] = timestamp_field
+        if day is not None:
+            self.__time['day'] = day
+        if hour is not None:
+            self.__time['hour'] = hour
+        if minute is not None:
+            self.__time['minute'] = minute
+        if second is not None:
+            self.__time['second'] = second
+
+        return self
 
     def set_fields(self, fields: Dict):
         self.__set_fields = fields
