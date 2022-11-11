5dbe3a003f3363e2c50bf5188a94a7698820152b
madkne-laptop <madkne@laptop>
2022-11-11 06:21:50 +0330

    update admin_api file in python interfaces

--- data/interfaces/python3/lib/apis/admin_api.py
+++ data/interfaces/python3/lib/apis/admin_api.py
@@ -1,6 +1,6 @@
 
 from typing import Dict, List, Tuple
-from base_api import BaseApi
+from .base_api import BaseApi
 
 
 class WorkflowAdminApi(BaseApi):
@@ -28,4 +28,14 @@ class WorkflowAdminApi(BaseApi):
         # print('response deploy:', response)
         if response.code == 200:
             return True, response.body['data']
+        return False, response.body['data']
+
+    def setFields(self, process_id: str, fields: Dict) -> Tuple[bool, str]:
+        response = self._callPOSTApi('/admin/workflow/set-fields', {
+            'process_id': process_id,
+            'fields': fields
+        })
+        print(response.body)
+        if response.code == 200:
+            return True, 'success'
         return False, response.body['data']
